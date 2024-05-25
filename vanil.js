46d8365ddba3ec23d62875be68e86277cb89419f
Galoshi2000 <152693987+Galoshi2000@users.noreply.github.com>
2024-05-25 17:58:54 +0600

    Add files via upload

---
+++ vanil.js
@@ -0,0 +1,70 @@
+// let numForMonth = +prompt("Введите номер месяца.От одного до двеннадцати");
+// let monthFoReal = parseInt(numForMonth)
+// switch(monthFoReal){
+//     case 1:
+//         console.log("Январь");
+//         break;
+//     case 2:
+//         console.log("Январь");
+//         break;
+//     case 3:
+//         console.log("Март");
+//         break;
+//     case 4:
+//         console.log("Апрель");
+//         break;
+//     case 5:
+//         console.log("Май");
+//         break;
+//     case 6:
+//         console.log("Июнь");
+//         break;
+//     case 7:
+//         console.log("Июль");
+//         break;
+//     case 8:
+//         console.log("Август");
+//         break;
+//     case 9:
+//         console.log("Сентябрь");
+//         break;
+//     case 10:
+//         console.log("Октябрь");
+//         break;
+//     case 11:
+//         console.log("Ноябрь");
+//         break;
+//     case 12:
+//         console.log("Декабрь");
+//         break;
+// }
+
+// let  monthForSeason = prompt("Введите месяц(с большой буквы и In Russian language плиз)")
+// switch(monthForSeason){
+//     case "Декабрь":
+//     case "Январь":
+//     case "Февраль":
+//         alert("Это Зима ＞﹏＜");
+//         break;
+//     case "Март":
+//     case "Апрель":
+//     case "Май":
+//         alert("Это Весна(～￣▽￣)～");
+//         break;
+//     case "Июнь":
+//     case "Июль":
+//     case "Август":
+//         alert("Это Лето (´▽`ʃ♡ƪ)");
+//         break;
+//     case "Сентябрь":
+//     case "Октябрь":
+//     case "Ноябрь":
+//         alert("Это Осень(╬▔皿▔)╯");
+//         break;
+// }
+
+let i = 1;
+while(i <= 100) {
+    console.log( i );
+    i++;
+}
